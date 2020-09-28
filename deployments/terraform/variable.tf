cfffcebf677e7bbaa7bdd6c54401650e4cfbaf2b
tuyalou <63755131+tuyalou@users.noreply.github.com>
2020-09-28 12:40:04 -0500

    Update variable.tf

--- deployments/terraform/variable.tf
+++ deployments/terraform/variable.tf
@@ -4,6 +4,10 @@ variable "deployment_image" {
 variable "deployment_environment" {
     default = "stage"
 }
+variable "deployment_name" {
+    default = "hello-world"
+}
+
 
 variable "deployment_endpoint" {
   type = "map"
